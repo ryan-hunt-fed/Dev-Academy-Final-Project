8467dfd9d2ead068d244f11ed87ca6f6da17d141
kristofferbacaling <kristofferbacaling@gmail.com>
2022-09-22 11:51:08 +1200

    kris and ming made database for pokehuman

---
+++ server/db/migrations/20220921231423_pokehumans.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('pokehumans', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.string('type_1')
+    table.string('type_2')
+    table.integer('HP')
+    table.integer('attack')
+    table.integer('defence')
+    table.integer('Sp_attack')
+    table.integer('Sp_defence')
+    table.integer('speed')
+    table.string('image')
+  })
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('pokehumans')
+};
