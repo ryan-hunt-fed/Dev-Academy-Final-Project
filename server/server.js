d46de16b58eaca8fd32a9ed6447b856d561283d4
Ming Shang Ng <mingshang@live.com>
2022-09-22 15:35:43 +1200

    remove the api routes

--- server/server.js
+++ server/server.js
@@ -4,11 +4,9 @@ const path = require('path')
 const getDexEntry = require('./routes/dexentry')
 
 const server = express()
-const pokehumansRoutes = require('./routes/pokehumans')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
-server.use('/v1/movie', pokehumansRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
