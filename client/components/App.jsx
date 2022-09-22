dfc2d6a511a0b853ea22a7811f0d9163f06eae24
Ryan Hunt <r-g-hunt@hotmail.com>
2022-09-22 13:27:25 +1200

    setup home component & starter styling for home

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
+import Home from './Home'
+
 function App() {
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        
       </header>
-      <section className="main">{/* add your code here */}</section>
+      <section className="main">
+        <Home />{/* add your code here */}
+      </section>
     </>
   )
 }
