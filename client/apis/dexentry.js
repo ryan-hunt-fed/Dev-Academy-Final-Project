5000af426a1236494fe0c6bb7559ddb6f6d4cec9
Jeff McNie <jeff.mcnie@gmail.com>
2022-09-22 15:10:12 +1200

    Send the data to the front end to be displayed

--- client/apis/dexentry.js
+++ client/apis/dexentry.js
@@ -2,5 +2,5 @@ import request from 'superagent'
 
 export async function getDexEntryAPI(id) {
   const entry = await request.get('/api/v1/dexentry/' + id)
-  return entry
+  return entry.body
 }
--- client/components/DexEntry.jsx
+++ client/components/DexEntry.jsx
@@ -7,14 +7,26 @@ export default function DexEntry() {
   const [pokehuman, setPokehuman] = useState()
   const { id } = useParams()
 
-  useEffect(() => {
-    setPokehuman(getDexEntryAPI(id))
-  }, [])
+  const handleClick = async () => {
+    const entry = await getDexEntryAPI(id)
+    console.log(entry)
+    setPokehuman(entry)
+  }
 
-  console.log(pokehuman)
   return (
     <>
       <div>DexEntry</div>
+      <button onClick={handleClick}>Click me</button>
+      <img src={pokehuman?.image} alt="A pokehuman!" />
+      <h1>{pokehuman?.name}</h1>
+      <h4>Type: {pokehuman?.type_1}</h4>
+      <h4>Type: {pokehuman?.type_2}</h4>
+      <h4>HP: {pokehuman?.HP}</h4>
+      <h4>Attack: {pokehuman?.attack}</h4>
+      <h4>Defence: {pokehuman?.defence}</h4>
+      <h4>Special Attack: {pokehuman?.Sp_attack}</h4>
+      <h4>Special Defence: {pokehuman?.Sp_defence}</h4>
+      <h4>Speed: {pokehuman?.speed}</h4>
     </>
   )
 }
