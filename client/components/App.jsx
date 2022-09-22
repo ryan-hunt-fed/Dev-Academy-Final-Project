9ca529bfa7317a63ad2d35a9390f21b349e4c387
Ryan Hunt <r-g-hunt@hotmail.com>
2022-09-22 14:49:45 +1200

    setup nav bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Routes, Route, } from 'react-router-dom'
 
 import Home from './Home'
+import Nav from './Nav'
+
 
 function App() {
   return (
     <>
       <header className="header">
-        
+       <Nav />
       </header>
       <section className="main">
-        <Home />{/* add your code here */}
       </section>
+      <Routes>
+         <Route path='/' element={<Home />}/>
+        {/* <Route path='/add' element={<Add />}/> */}
+        {/* <Route path='/dex' element={<Dex />}/> */}
+        {/* <Route path='/dex/:id' element={<DexEntry />}/> */}
+      </Routes>
     </>
   )
 }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
       <div className='intro-container'>
       <h4 className='intro'>Welcome to PokeHuman,</h4> 
       <div className='intro-text-container'>
-      <p className='intro'>Pokemon have finally risen up and have decided to be the new Pokemon masters. They have created a new Pokedex filled with Pokehumans and much like their previous rulers have chosen to make their Pokehumans fight one another. In this app you can view and add Pokehumans to the Dex, you can then set your Pokehumans against each other in the battle games</p>
+      <p className='intro'>Pokemon have finally risen up and have decided to be the new Pokemon masters. They have created a new Pokedex filled with Pokehumans and much like their previous rulers have chosen to make their Pokehumans fight one another. In this app you can view and add Pokehumans to the Dex, then set your Pokehumans against each other in the battle games</p>
       </div>
       </div>
     </>
