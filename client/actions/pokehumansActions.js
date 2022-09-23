db44d8e27466d133e35a08c144c9c2ce44d6ddfc
Jeff McNie <107101266+jeff-mcnie@users.noreply.github.com>
2022-09-23 15:10:31 +1200

    Revert "Jeffy"

--- client/actions/pokehumansActions.js
+++ client/actions/pokehumansActions.js
@@ -1,4 +1,4 @@
-import { addPokeHumanApi, getAllPokesApi } from '../apis/pokehumans'
+import { addPokeHumanApi, getAllPokesApi } from '../apis/pokehumansApi'
 
 //Variable
 
--- client/reducers/pokehumansReducers.js
+++ client/reducers/pokehumansReducers.js
@@ -1,4 +1,4 @@
-import { ADD_POKEHUMANS, GET_POKEHUMANS } from '../actions/pokehumans'
+import { ADD_POKEHUMANS, GET_POKEHUMANS } from '../actions/pokehumansActions'
 
 const reducer = (state = [], action) => {
   const { type, payload } = action
