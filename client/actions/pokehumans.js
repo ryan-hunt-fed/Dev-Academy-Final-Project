92e7b53430ee406a1cab1ca01c7116794ef39496
Jeff McNie <jeff.mcnie@gmail.com>
2022-09-23 15:08:41 +1200

    Rename files, add teamselect jsx

---
+++ client/actions/pokehumans.js
@@ -0,0 +1,46 @@
+import { addPokeHumanApi, getAllPokesApi } from '../apis/pokehumans'
+
+//Variable
+
+export const GET_POKEHUMANS = 'GET_POKEHUMANS'
+export const ADD_POKEHUMANS = 'ADD_POKEHUMANS'
+
+//Action Creator
+
+function getPokehumansAction(humans) {
+  return {
+    type: GET_POKEHUMANS,
+    payload: humans,
+  }
+}
+
+function addPokeHumanAction(human) {
+  return {
+    type: ADD_POKEHUMANS,
+    payload: human,
+  }
+}
+
+//THUNK
+
+export function getAllPokehumansThunk() {
+  // api call
+  // dispatch action
+
+  return async (dispatch) => {
+    try {
+      const humans = await getAllPokesApi()
+      dispatch(getPokehumansAction(humans))
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+}
+
+export function postAddPokehumanThunk(data) {
+  return (dispatch) => {
+    return addPokeHumanApi(data).then((human) => {
+      dispatch(addPokeHumanAction(human))
+    })
+  }
+}
