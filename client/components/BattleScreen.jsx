883d8f0128494160bbbec9f4b746c71e52a7aa10
Ryan Hunt <r-g-hunt@hotmail.com>
2022-09-26 12:08:33 +1300

    refactered to one on one

--- client/components/BattleScreen.jsx
+++ client/components/BattleScreen.jsx
@@ -5,16 +5,16 @@ import { useLocation } from 'react-router-dom'
 import { getAllPokehumansThunk } from '../actions/pokehumans'
 
 import PokeHumanOne from './PokeHumanOne'
-import PokeHumanTwo from './PokeHumanTwo'
-import PokeHumanThree from './PokeHumanThree'
+// import PokeHumanTwo from './PokeHumanTwo'
+// import PokeHumanThree from './PokeHumanThree'
 import AiPokehumanOne from './AiPokehumanOne'
-import AiPokehumanThree from './AiPokehumanThree'
-import AiPokehumanTwo from './AiPokehumanTwo'
+// import AiPokehumanThree from './AiPokehumanThree'
+// import AiPokehumanTwo from './AiPokehumanTwo'
 
 export default function BattleScreen() {
   const location = useLocation()
   const dispatch = useDispatch()
-  const humans = useSelector((store) => store.pokehumans)
+  const pokehumans = useSelector((store) => store.pokehumans)
   const [aiTeam, setAiTeam] = useState([])
 
   useEffect(() => {
@@ -29,7 +29,7 @@ export default function BattleScreen() {
 
   function generateAiTeam(e) {
     e.preventDefault()
-    setAiTeam(getMultipleRandom(humans, 3))
+    setAiTeam(getMultipleRandom(pokehumans, 2))
   }
 
   // Potential turn taking system
@@ -43,25 +43,98 @@ export default function BattleScreen() {
   // AI needs to use moves to deal damage
   // player needs to use moves to deal damage 
   // We need to target hp data and take away damage result 
-  // The moves need damage assigned to them 
+  // The moves need damage assigned to them - done
   // We need to work out if the move hits or not
 
-  
+  const physicalMoveArr = [
+    'Tackle',
+    'Pound',
+    'Thunder Punch',
+    'Low Kick',
+    'Fly',
+    'Mega Kick',
+  ]
+
+  const specialMoveArr = [
+    'Flamethrower',
+    'Hyper Beam',
+    'Psychic',
+    'Solar Beam',
+    'Moonblast',
+    'Ice Beam',
+  ]
+
+  const physicalMove = physicalMoveArr[Math.floor(Math.random() * physicalMoveArr.length)] 
+  const specialMove = specialMoveArr[Math.floor(Math.random() * specialMoveArr.length)]
+
+  let physicalDamage = 1
+
+  const physicalDamageCalc = () => {
+    if (location.state[0].attack > 75) {
+      return (physicalDamage = 4)
+    } else if (location.state[0].attack > 50) {
+      return (physicalDamage = 3)
+    } else if (location.state[0].attack > 25) {
+      return (physicalDamage = 2)
+    } else {
+      return physicalDamage
+    }
+  }
+
+  let specialDamage = 1
+
+  const specialDamageCalc = () => {
+    if (location.state[0].spAttack > 75) {
+      return (specialDamage = 4)
+    } else if (location.state[0].spAttack > 50) {
+      return (specialDamage = 3)
+    } else if (location.state[0].spAttack > 25) {
+      return (specialDamage = 2)
+    } else {
+      return specialDamage
+    }
+  }
+
+
+  const [alive, setAlive] = useState(true)
+  const [active, setActive] = useState(true)
   
 
   // pokehuman dies
-//  const faint = () => {
+ const faint = () => {
 
   //player health 
-  if (pokehuman.HP <= 0 ) {
-    console.log(pokehuman.name, 'has died')
-  }
-  //Ai health
-  else if (pokehuman.HP <= 0) {
-    console.log(pokehuman.name, 'has died')
+  if (pokehumans.HP <= 0 ) {
+    // console.log(pokehuman.name, 'has died')
+    setAlive(false)
+    for (i = 0; i = pokehumans.length; i++){
+      if (alive = false) {
+        console.log('You Lose')
+      }
+    }
   }
 
-//  }
+  //Ai health - need to differentiate between pokehuman ai and player
+  else if (pokehumans.HP <= 0) {
+    // console.log(pokehuman.name, 'has died')
+    setAlive(false)
+    if (alive = false) {
+      console.log('You Win')
+    }
+  }
+
+ }
+
+ //switch pokehumans
+function switchPokehuman () {
+  if (active = false) {
+    return setActive(true)
+  }
+  else if (active = true){
+    return setActive(false)
+  }
+ 
+}
 
 
   
@@ -72,8 +145,17 @@ export default function BattleScreen() {
       {location.state && (
         <>
           <PokeHumanOne pokehuman={location.state[0]} />
-          <PokeHumanTwo pokehuman={location.state[1]} />
-          <PokeHumanThree pokehuman={location.state[2]} />
+          <button>
+          {physicalMove}
+          {physicalDamageCalc()}
+        </button>
+        <button>
+          {specialMove}
+          {specialDamageCalc()}
+        </button>
+          
+          {/* <PokeHumanTwo pokehuman={location.state[1]} /> */}
+          {/* <PokeHumanThree pokehuman={location.state[2]} /> */}
         </>
       )}
       <div>
@@ -89,12 +171,12 @@ export default function BattleScreen() {
         {aiTeam.length > 1 && (
           <>
             <AiPokehumanOne pokehuman={aiTeam[0]} />
-            <AiPokehumanTwo pokehuman={aiTeam[1]} />
-            <AiPokehumanThree pokehuman={aiTeam[2]} />
+            {/* <AiPokehumanTwo pokehuman={aiTeam[1]} /> */}
+            {/* <AiPokehumanThree pokehuman={aiTeam[2]} /> */}
           </>
         )}
       </div>
-      <button onClick={generateAiTeam}>Generate Team</button>
+      <button onClick={generateAiTeam}>Generate Opponents Team</button>
     </>
   )
 }
